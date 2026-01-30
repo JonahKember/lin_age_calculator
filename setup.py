cb48e71de8ce901f4c84ffdcf290825de4361205
Jonah Kember <jonah.kember@mail.mcgill.ca>
2026-01-30 09:41:34 -0500

    added MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lin_age_calculator",
-    version="0.1.0",
+    name='lin_age_calculator',
+    version='0.1.0',
+
     packages=find_packages(),
-    install_requires=[],
+    include_package_data=True,
+    package_data={
+        'calculator': ['models/*.csv'],
+    },
+
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
 )
